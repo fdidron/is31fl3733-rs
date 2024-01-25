158b8dfd25296a0e5630cdd08a289561cc689e7c
Alon <alon@Alons-MacBook-Pro.local>
2024-01-26 01:43:29 +0200

    Removed RawDevice abstraction and refactored code to support dual async/blocking variants

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,5 @@ mod state;
 mod is31fl3733;
 pub use is31fl3733::{IS31FL3733Error, IS31FL3733};
 
-mod device;
-pub use device::RawDevice;
-
-mod i2c;
-
 #[cfg(test)]
 mod test_utils;
--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,29 +1,120 @@
-use embedded_hal_async::i2c::{Error, ErrorKind, ErrorType};
+use crate::is31fl3733::{Async, Blocking, Mode};
 
 #[derive(Debug)]
-pub enum FakeI2cError {
+pub enum FakeI2cError<M: Mode> {
     Error,
+    #[allow(non_camel_case_types)]
+    _phantom(core::marker::PhantomData<M>),
 }
-impl Error for FakeI2cError {
-    fn kind(&self) -> ErrorKind {
-        ErrorKind::Other
+
+impl embedded_hal_async::i2c::Error for FakeI2cError<Async> {
+    fn kind(&self) -> embedded_hal_async::i2c::ErrorKind {
+        embedded_hal_async::i2c::ErrorKind::Other
     }
 }
 
-pub struct FakeI2cBus<const N: usize, const M: usize> {
+impl embedded_hal::i2c::Error for FakeI2cError<Blocking> {
+    fn kind(&self) -> embedded_hal::i2c::ErrorKind {
+        embedded_hal::i2c::ErrorKind::Other
+    }
+}
+
+impl<const N: usize, const M: usize> embedded_hal::i2c::I2c
+    for FakeI2cBus<N, M, Blocking>
+{
+    fn transaction(
+        &mut self,
+        _address: embedded_hal_async::i2c::SevenBitAddress,
+        operations: &mut [embedded_hal_async::i2c::Operation<'_>],
+    ) -> Result<(), Self::Error> {
+        for operation in operations {
+            match operation {
+                embedded_hal_async::i2c::Operation::Write(write) => {
+                    self.write_data
+                        .extend_from_slice(write)
+                        .map_err(|_| FakeI2cError::Error)?;
+                }
+                embedded_hal_async::i2c::Operation::Read(read) => {
+                    // Copy read.len() bytes from read_data to read and remove those bytes
+                    for i in 0..read.len() {
+                        read[i] = self.read_data.remove(i);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<const N: usize, const M: usize> embedded_hal_async::i2c::I2c
+    for FakeI2cBus<N, M, Async>
+{
+    async fn transaction(
+        &mut self,
+        _address: embedded_hal_async::i2c::SevenBitAddress,
+        operations: &mut [embedded_hal_async::i2c::Operation<'_>],
+    ) -> Result<(), Self::Error> {
+        for operation in operations {
+            match operation {
+                embedded_hal_async::i2c::Operation::Write(write) => {
+                    self.write_data
+                        .extend_from_slice(write)
+                        .map_err(|_| FakeI2cError::Error)?;
+                }
+                embedded_hal_async::i2c::Operation::Read(read) => {
+                    // Copy read.len() bytes from read_data to read and remove those bytes
+                    for i in 0..read.len() {
+                        read[i] = self.read_data.remove(i);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+pub struct FakeI2cBus<const N: usize, const M: usize, MODE: Mode> {
     pub write_data: heapless::Vec<u8, N>,
     pub read_data: heapless::Vec<u8, M>,
+    _phantom: core::marker::PhantomData<MODE>,
 }
 
-impl<const N: usize, const M: usize> ErrorType for FakeI2cBus<N, M> {
-    type Error = FakeI2cError;
+impl<const N: usize, const M: usize> embedded_hal_async::i2c::ErrorType
+    for FakeI2cBus<N, M, Blocking>
+{
+    type Error = FakeI2cError<Blocking>;
 }
 
-impl<const N: usize, const M: usize> FakeI2cBus<N, M> {
+impl<const N: usize, const M: usize> embedded_hal_async::i2c::ErrorType
+    for FakeI2cBus<N, M, Async>
+{
+    type Error = FakeI2cError<Async>;
+}
+
+impl<const N: usize, const M: usize> FakeI2cBus<N, M, Blocking> {
+    pub fn new_blocking() -> Self {
+        Self {
+            write_data: heapless::Vec::new(),
+            read_data: heapless::Vec::new(),
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+impl<const N: usize, const M: usize> FakeI2cBus<N, M, Async> {
+    pub fn new_async() -> Self {
+        Self {
+            write_data: heapless::Vec::new(),
+            read_data: heapless::Vec::new(),
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+impl<const N: usize, const M: usize, MODE: Mode> FakeI2cBus<N, M, MODE> {
     pub fn new() -> Self {
         Self {
             write_data: heapless::Vec::new(),
             read_data: heapless::Vec::new(),
+            _phantom: core::marker::PhantomData,
         }
     }
 
@@ -31,6 +122,7 @@ impl<const N: usize, const M: usize> FakeI2cBus<N, M> {
         Self {
             write_data: heapless::Vec::new(),
             read_data: heapless::Vec::from_slice(read_data).unwrap(),
+            _phantom: core::marker::PhantomData,
         }
     }
 
